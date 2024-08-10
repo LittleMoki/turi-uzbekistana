eb620fb126cbb47f52c043812ccd797660b45e36
LittleMoki <steamlittle659@gmail.com>
2024-08-11 02:08:42 +0500

    change api

--- frontend/src/Api/api.js
+++ frontend/src/Api/api.js
@@ -5,6 +5,6 @@ export const api = axios.create({
 	httpsAgent: new https.Agent({
 		rejectUnauthorized: false
 	}),
-baseURL: 'http://localhost:4000/',
-	// baseURL: 'https://api.turi-uzbekistana.ru/',
+// baseURL: 'http://localhost:4000/',
+	baseURL: 'https://api.turi-uzbekistana.ru/',
 })
